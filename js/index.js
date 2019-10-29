ec2a610167da0c74c7879c6ee02389147f442e4d
Indigo Richards <thedomesticdingo@gmail.com>
2019-10-29 16:50:57 -0700

    mvp done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,55 @@
 // Your code goes here
+let stopLink = document.querySelectorAll(".nav-link").forEach(x =>{
+    x.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("Stopped the link.");
+    })
+})
+
+  
+  
+let dblhead = document.querySelector(".logo-heading");
+
+dblhead.addEventListener("dblclick", (x) => {
+    dblhead.style.color = "#00CED1"
+    console.log("Clik'd");
+  event.stopPropagation();
+  })
+  
+let topimg = document.querySelector(".intro img");
+topimg.addEventListener("click", () => {
+    topimg.src = "img/bus.png"
+})
+
+let yellowheaders = document.querySelectorAll(".text-content h2").forEach(x =>{
+    x.addEventListener("mouseover", () => {
+        x.style.color = "yellow";
+    })
+})
+
+const body = document.querySelector("body");
+
+body.addEventListener("wheel", () => {
+ body.style.backgroundColor="grey";
+})
+
+const navbar = document.querySelector(".main-navigation");
+navbar.addEventListener("mouseenter", () => {
+    navbar.style.backgroundColor = "blue";
+})
+navbar.addEventListener("mouseleave", () => {
+    navbar.style.backgroundColor = "";
+})
+
+window.addEventListener("load", () => {
+    console.log("The page has loaded.")
+})
+
+window.addEventListener("resize", () => {
+    console.log("The window was resized.")
+})
+
+let footer = document.querySelector(".footer p");
+footer.addEventListener("contextmenu", () => {
+    footer.style.color = "yellow";
+})
